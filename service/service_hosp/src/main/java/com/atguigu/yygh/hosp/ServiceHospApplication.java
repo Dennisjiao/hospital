419fa19407ff5e8ed68dae4cc67a20a87d04201d
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-03-30 16:14:34 +0100

    environment build

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ServiceHospApplication.class, args);
+
+    }
+}
