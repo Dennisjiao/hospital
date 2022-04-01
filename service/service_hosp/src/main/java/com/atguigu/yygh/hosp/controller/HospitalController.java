84be07f01da0a8b6cd3999870a0b0ef16151c812
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-04-01 20:22:45 +0100

    Swagger2Config (finish)

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalController.java
@@ -2,11 +2,15 @@ package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalController {
@@ -18,6 +22,7 @@ public class HospitalController {
     //增删改查方法
 
     //查询医院设置表中的所有数据
+    @ApiOperation(value = "获取医院所有设置信息")
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet() {
         //调用Service中的方法
@@ -26,6 +31,7 @@ public class HospitalController {
     }
 
     //删除医院设置(逻辑删除)
+    @ApiOperation(value = "逻辑删除医院设置信息")
     @DeleteMapping("{id}")
     public boolean removeHospitalSet(@PathVariable Long id){
         boolean flag = hospitalSetService.removeById(id);
