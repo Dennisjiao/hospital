462c72428cc68053d3a74b4817a8c5257b0a055c
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-04-01 20:08:45 +0100

    Swagger2Config (finish)

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -2,8 +2,11 @@ package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@ComponentScan("com.atguigu")
 public class ServiceHospApplication {
     public static void main(String[] args){
         SpringApplication.run(ServiceHospApplication.class, args);
