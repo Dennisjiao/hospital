5854e4c1d9b0a2cab00917d148d5a599e6c0b809
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-04-25 09:47:17 +0100

    添加端口冲突解决注解

--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/controller/DictController.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/controller/DictController.java
@@ -7,16 +7,14 @@ import com.atguigu.yygh.model.cmn.Dict;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/admin/smn/dict")
 @Api(value ="数据字典")
+@CrossOrigin
 public class DictController {
 
 
