8e1ea8c16f993849515712374d431552db8954dd
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-06-12 01:53:27 +0100

    安装nacos 引入依赖并配置端口，启动类加入@EEnableDiscoveryClient注解

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -2,11 +2,13 @@ package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @ComponentScan("com.atguigu")
+@EnableDiscoveryClient
 public class ServiceHospApplication {
     public static void main(String[] args){
         SpringApplication.run(ServiceHospApplication.class, args);
